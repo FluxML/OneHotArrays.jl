bf3c51d8ae39deab50902b8d6950e05d1e6a3f53
Kyle Daruwalla <daruwalla@wisc.edu>
2022-04-05 08:52:56 -0500

    Fix typo in `docs/make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,9 @@
 using Documenter, OneHotArrays
 
 DocMeta.setdocmeta!(OneHotArrays, :DocTestSetup, :(using OneHotArrays); recursive = true)
-make(sitename = "OneHotArrays", doctest = false,
-     pages = ["Overview" => "index.md",
-              "Reference" => "reference.md"])
+makedocs(sitename = "OneHotArrays", doctest = false,
+         pages = ["Overview" => "index.md",
+                  "Reference" => "reference.md"])
 
 deploydocs(repo = "github.com/FluxML/OneHotArrays.jl.git",
            target = "build",
