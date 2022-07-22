83cfeece4dd9a9ea715d2513bd0f62dfe6eb4338
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-07-22 14:26:10 -0400

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,18 @@
 using Documenter, OneHotArrays
 
 DocMeta.setdocmeta!(OneHotArrays, :DocTestSetup, :(using OneHotArrays); recursive = true)
-makedocs(sitename = "OneHotArrays", doctest = false,
+
+makedocs(sitename = "OneHotArrays.jl",
+         doctest = false,
          pages = ["Overview" => "index.md",
-                  "Reference" => "reference.md"])
+                  "Reference" => "reference.md"],
+         format = Documenter.HTML(
+              canonical = "https://fluxml.ai/OneHotArrays.jl/stable/",
+              # analytics = "UA-36890222-9",
+              assets = ["assets/flux.css"],
+              prettyurls = get(ENV, "CI", nothing) == "true"
+         ),
+)
 
 deploydocs(repo = "github.com/FluxML/OneHotArrays.jl.git",
            target = "build",
