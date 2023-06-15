9e5bec81dde7f840cc94ff7718ed27adaa6ab363
CarloLucibello <carlo.lucibello@gmail.com>
2023-06-15 16:00:41 +0200

    allow NNlib v0.9

--- test/gpu.jl
+++ test/gpu.jl
@@ -23,7 +23,11 @@ end
   @test (repr("text/plain", y); true)
 
   gA = rand(3, 2) |> cu;
-  @test_broken gradient(A -> sum(A * y), gA)[1] isa CuArray  # fails with JLArray, bug in Zygote?
+  if VERSION >= v"1.9"
+    @test gradient(A -> sum(A * y), gA)[1] isa CuArray  # fails with JLArray, bug in Zygote?
+  else
+    @test_broken gradient(A -> sum(A * y), gA)[1] isa CuArray
+  end
 end
 
 @testset "onehotbatch(::CuArray, ::UnitRange)" begin
